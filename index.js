4e48a574ab383f06e586558859be600e553dc35e
max martinez <mmfern2003@gmail.com>
2023-12-13 15:22:24 -0500

    import jester, declare userInput questions, validate user inputs to be what is being required

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const inquirer = require('inquirer');
+const jest = require('jest');
+
+const userInput = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter a 3 character word:',
+        validate: function(value) {
+            // Checking if the input is 3 characters long or less
+            if (value.length <= 3) {
+                return true;
+            }
+            return 'Please enter a word no more than 3 characters.';
+        }
+    },
+    {
+        type: 'input',
+        name: 'color',
+        message: 'Enter a color (name or hexadecimal):',
+        validate: function(value) {
+            const hexColorRange = /^#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/;
+
+            if (value === '' || hexColorRange.test(value) || typeof value === 'string') {
+                return true;
+            }
+            return 'Please enter a valid color name or hexadecimal.';
+        }
+    }
+];
